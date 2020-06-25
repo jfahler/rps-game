006866ff1fa3d8af19ea45e807df79f1dbffaf2c
jfahler <jfahler@gmail.com>
2020-06-25 15:01:46 -0500

    deleted:    df 	deleted:    images/brain.png 	deleted:    images/ui.png 	modified:   index.html 	modified:   main.css 	modified:   rps-game.js

--- rps-game.js
+++ rps-game.js
@@ -19,47 +19,47 @@ function game() {
             alert("Oof. You lost."); 
         }
                 
-function computerPlay(){
-    randomNumber(1,3); 
-    function randomNumber(min, max) {
-        R0 = Math.floor(Math.random() * (max - min + 1) + min); 
-        }
-        if (R0 == 1){
-            return "rock"; 
-        }
-        else if (R0 == 2){
-            return "paper"; 
-        }
-        else {
-            return "scissors"; 
-        }   
-}
+    function computerPlay(){
+        randomNumber(1,3); 
+        function randomNumber(min, max) {
+            R0 = Math.floor(Math.random() * (max - min + 1) + min); 
+            }
+            if (R0 == 1){
+                return "rock"; 
+            }
+            else if (R0 == 2){
+                return "paper"; 
+            }
+            else {
+                return "scissors"; 
+            }   
+    };
 
-function playRound(playerSelection,computerSelection) {
-    prelimResult = 0; 
-    
-    if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock" || 
-    playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors" ||
-    playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
-        prelimResult = 1; 
-        return "You won this rond! " + playerSelection.toLowerCase() + " beats " + computerSelection.toLowerCase() + " ."
-    }
-    else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper" || 
-    playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock" ||
-    playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors"){
+    function playRound(playerSelection,computerSelection) {
         prelimResult = 0; 
-        return "You tied this round! " + playerSelection.toLowerCase() + " is the same as " + computerSelection.toLowerCase() + " ."
-    }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors" || 
-    playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper" ||
-    playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock"){
-        prelimResult = -1; 
-        return "You lost this round! " + playerSelection.toLowerCase() + " beats " + computerSelection.toLowerCase() + " ."
-    }
-}
+        
+        if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock" || 
+        playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors" ||
+        playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
+            prelimResult = 1; 
+            return "You won this rond! " + playerSelection.toLowerCase() + " beats " + computerSelection.toLowerCase() + " ."
+        }
+        else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper" || 
+        playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock" ||
+        playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors"){
+            prelimResult = 0; 
+            return "You tied this round! " + playerSelection.toLowerCase() + " is the same as " + computerSelection.toLowerCase() + " ."
+        }
+        else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors" || 
+        playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper" ||
+        playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock"){
+            prelimResult = -1; 
+            return "You lost this round! " + playerSelection.toLowerCase() + " beats " + computerSelection.toLowerCase() + " ."
+        }
+    };
 
                 
-}
+};
 
 game()
 
